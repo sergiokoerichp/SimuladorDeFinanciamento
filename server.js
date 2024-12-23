3387881f3052eed26e584dea0c83cf4c9ef4aaf4
Sérgio Koerich Pereira <129325112+sergiokoerich@users.noreply.github.com>
2024-12-23 03:00:47 -0300

    Corrigindo configuração do package.json para deploy no Heroku

--- server.js
+++ server.js
@@ -87,11 +87,10 @@ app.post('/api/calcular', (req, res) => {
 
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'build')));
-
+app.use(express.static(path.join(__dirname, 'capital-giro-frontend', 'build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'capital-giro-frontend', 'build', 'index.html'));
 });
 
   const PORT = process.env.PORT || 5000;
